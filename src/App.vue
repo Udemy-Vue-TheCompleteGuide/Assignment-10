90bdd932bd81a44fb3d04093f8832036fd388485
Marlon Fernandez <marlonfsolis@outlook.com>
2020-08-31 21:31:11 -0400

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives Exercise</h1>
+        <!-- Exercise -->
+        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+        <button class="btn btn-success" v-my-on:click="onClick" v-my-on:leave="onMouseLeave">Click Me</button>
+        <button class="btn btn-success" v-my-on:dblclick="onDblClick">Double Click Me</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyOnDirective from "@/directives/MyOn.directive.js";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  directives: {
+    'MyOn': MyOnDirective,
+    // 'MyOn1': {
+    //   bind: (el: HTMLBaseElement, binding: any, vnode) => { /* Implementation...*/ }
+    // }
+  },
+  methods: {
+    onClick(ev) {
+      alert(`The button ${ev.target.outerText} was clicked!`);
+    },
+
+    onDblClick(ev) {
+      alert(`The button ${ev.target.outerText} was double clicked!`);
+    },
+
+    onMouseLeave(ev) {
+      alert(`The cursor just leave the button ${ev.target.outerText}`);
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button {
+  margin-right: 10px;
 }
 </style>
